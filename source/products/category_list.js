89d014b6584a7876fffca546b21a6cd720a7b273
pangqilong <1223750634@qq.com>
2018-06-05 18:23:31 +0800

    商品分类接口文档返回内容补充

--- source/products/category_list.js
+++ source/products/category_list.js
@@ -2,38 +2,45 @@
   "data": {
     "categories": [
       {
+        "cover": "http://xxx/_uploads/photos/180530/72de0b9ca1ae5a3.jpg",
         "description": "",
         "id": 1,
         "name": "家居",
+        "pid": 0,
         "sort_order": 1,
         "status": 1
       },
       {
+        "cover": "http://xxx/_uploads/photos/180530/72de0b9ca1ae5a3.jpg",
         "description": "",
         "id": 3,
         "name": "创意设计",
+        "pid": 0,
         "sort_order": 3,
         "status": 1
       },
       {
+        "cover": "http://xxx/_uploads/photos/180530/72de0b9ca1ae5a3.jpg",
         "description": "",
         "id": 4,
         "name": "杯子",
+        "pid": 0,
         "sort_order": 2,
         "status": 1
       },
       {
+        "cover": "http://xxx/_uploads/photos/180530/72de0b9ca1ae5a3.jpg",
         "description": "",
         "id": 5,
         "name": "智能硬件",
+        "pid": 0,
         "sort_order": 1,
         "status": 1
       }
-
     ],
     "count": 17,
-    "next": "http://127.0.0.1:9000/api/v1.0/categories?page=2",
-    "prev": null
+    "next": false,
+    "prev": false
   },
   "status": {
     "code": 200,
