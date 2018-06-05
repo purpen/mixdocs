6440272ef4dae63348cb6f59d5555ecf65fb25ac
pangqilong <1223750634@qq.com>
2018-06-05 18:24:04 +0800

    增加分销商品返回内容文档

---
+++ source/products/distribution_product_list.js
@@ -0,0 +1,55 @@
+{
+  "data": {
+    "count": 54,
+    "next": true,
+    "prev": false,
+    "products": [
+      {
+        "commission_price": "佣金",
+        "commission_rate": "佣金比",
+        "cost_price": "采购价",
+        "cover": "封面图片链接地址",
+        "description": "描述",
+        "features": "商品推荐语",
+        "id_code": "商品的69码",
+        "is_distributed": "是否分销商品",
+        "is_proprietary": "是否自营商品",
+        "name": "商品名",
+        "rid": "商品编号",
+        "price": "零售价",
+        "s_height": "高度",
+        "s_length": "长度",
+        "s_weight": "重量",
+        "s_width": "宽度",
+        "sale_price": "促销价",
+        "sticked": "是否推荐",
+        "stock_count": "库存数"
+      },
+      {
+        "commission_price": 28,
+        "commission_rate": 14.00,
+        "cost_price": "采购价",
+        "cover": "http://xxx/_uploads/photos/171010/fe7380064e18135.jpg",
+        "description": "描述",
+        "id_code": null,
+        "is_distributed": true,
+        "is_proprietary": false,
+        "name": "速腾套装",
+        "rid": "117210372661",
+        "price": 0,
+        "s_height": 0,
+        "s_length": 0,
+        "s_weight": 0,
+        "s_width": 0,
+        "sale_price": 200,
+        "sticked": null,
+        "stock_count": 35
+      },
+    ]
+  },
+  "status": {
+    "code": 200,
+    "message": "Ok all right."
+  },
+  "success": true
+}
