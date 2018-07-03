e8dc037bd8f3d9e4470da2b6bf73443debe39a1f
Junyi <Junyi@JunyideMacBook-Pro.local>
2018-07-03 17:38:00 +0800

    添加订单

--- source/orders/order_list.js
+++ source/orders/order_list.js
@@ -4,56 +4,54 @@
         "next": "http://0.0.0.0:9000/api/v1.0/orders?status=2&page=2",  // 下一页链接
         "orders": [
             {
-                "bonus_amount": 0,  // 红包金额
-                "buyer_address": "朝阳区",  // 买家地址
-                "buyer_city": "北京",  // 买家城市
+                "buyer_address": "青年路",  // 买家地址
+                "buyer_city": "淄博",  //买家市
                 "buyer_country": "中国",  // 买家国家
-                "buyer_name": "田帅",  // 买家姓名
-                "buyer_phone": "13278989898",  // 买家手机
-                "buyer_province": "北京",  // 买家省
+                "buyer_name": "ZT-2",
+                "buyer_phone": "13260180689",  // 买家电话
+                "buyer_province": "山东",  // 买家省
                 "buyer_remark": null,  // 买家备注
-                "buyer_tel": null,  // 买家电话
-                "buyer_zipcode": null,  // 买家邮政编码
-                "coupon_amount": 2,  // 优惠券金额
-                "created_at": 1528620896,  // 订单时间
-                "customer_order_id": null,  // 关联分销商订单编号
-                "discount_amount": 0,  // 总优惠金额
-                "express_at": 0,  // 发货时间
-                "express_name": "",  // 快递公司
-                "express_no": null,  // 运单号
-                "first_discount": 300,  // 首单优惠金额
-                "freight": 0,  // 运费
-                "items": [
-                    {
-                        "cost_price": "1.00",  // 采购价
-                        "cover": "http://0.0.0.0:9000/_uploads/photos/1",  // 商品图片
-                        "deal_price": 999.1,  // 交易单价
-                        "discount_amount": 0,  // 优惠金额
-                        "id_code": "1",  // 商品编码
-                        "mode": "1 1",  // 型号
-                        "price": "1.00",  // 零售价
-                        "product_name": "摩托",  // 商品名
-                        "quantity": 3,  // 购买数量
-                        "rid": "1",  // sku_serial_no
-                        "s_color": "1",  // 颜色
-                        "s_model": "1",  // 型号
-                        "s_weight": "1.00",  // 重量
-                        "sale_price": "1.00",  // 促销价
-                        "stock_count": 11089  // 库存
-                    },
-                ],
-                "outside_target_id": "D18061062954387",  // 第三方平台订单编号
-                "pay_amount": 3996.4,  // 支付金额
+                "buyer_tel": "13260180689",  // 买家手机
+                "buyer_zipcode": "255300",  // 买家邮编
+                "coupon_amount": 0,  // 优惠券金额
+                "created_at": 1530608616,  // 创建时间
+                "customer_order_id": null,  // 分销商订单编号
+                "discount_amount": 0,  // 店铺优惠金额 = 首单优惠 + 满减 + 优惠券
+                "first_discount": 0,  // 首单优惠
+                "freight": 14,  // 运费
+                "official_order_id": null,  // 官方平台订单号
+                "outside_target_id": "D18070316803529",  // 第三方平台订单号
+                "pay_amount": 17,  // 支付金额
                 "reach_minus": 0,  // 满减金额
                 "received_at": 0,  // 收货时间
                 "remark": null,  // 卖家备注
-                "rid": "D18061062954387",  // 订单编号
-                "ship_mode": 1,  // 物流方式 1、快递；2、自提；
-                "status": 40,  // 订单状态
+                "rid": "D18070316803529",  // 订单编号
+                "ship_mode": 1,  // 运送方式
+                "status": 5,  // 订单状态
                 "store_name": "第一家(None)",  // 店铺名称
-                "total_amount": 3996.4,  // 订单总金额
-                "total_quantity": 4  // 总数量
-            },
+                "total_amount": 3,  // 商品金额
+                "total_quantity": 3  // 商品总数量
+                "items": [
+                    {
+                        "cover": "http://0.0.0.0:9000/_uploads/photos/1",
+                        "deal_price": 1,  // 交易价格
+                        "express": 4,  // 快递公司ID
+                        "express_at": 0,  // 发货时间
+                        "express_no": null,  // 运单号
+                        "id_code": "1",
+                        "mode": "1 1",
+                        "price": 1,
+                        "product_name": "摩托",  // 商品名
+                        "quantity": 3,  // 数量
+                        "rid": "1",  // sku
+                        "s_color": "1",
+                        "s_model": "1",
+                        "s_weight": 1,
+                        "sale_price": 1,
+                        "stock_count": 11020
+                    }
+                ],
+        },
         ],
         "prev": null  // 上一页链接
     },
